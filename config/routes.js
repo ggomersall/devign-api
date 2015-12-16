9077d25884ab410ea57b5f003af80e609cc55e93
Gareth Gomersall <ggomersall@gmail.com>
2015-12-16 11:23:00 +0000

    edited to route to include routes for the post function

--- config/routes.js
+++ config/routes.js
@@ -4,6 +4,7 @@ var passport    = require('passport');
 
 var usersController = require('../controllers/usersController');
 var authController = require('../controllers/authController');
+var ideasController = require('../controllers/ideasController');
 
 router.post('/login', authController.login);
 router.post('/signup', authController.signup);
@@ -11,11 +12,21 @@ router.post('/signup', authController.signup);
 router.route('/users')
   .get(usersController.usersIndex);
 
+router.route('/ideas')
+  .get(ideasController.ideasIndex)
+  .post(ideasController.ideaCreate);
+
 router.route('/users/:id')
   .get(usersController.userShow)
   .put(usersController.userUpdate)
   .patch(usersController.userUpdate)
   .delete(usersController.userDelete);
+
+router.route('/ideas/:id')
+  .get(ideasController.ideaShow)
+  .put(ideasController.ideaUpdate)
+  .patch(ideasController.ideaUpdate)
+  .delete(ideasController.ideaDelete);
 
 
 module.exports = router;
